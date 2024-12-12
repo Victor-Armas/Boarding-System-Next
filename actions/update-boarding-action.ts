7a641e80f9e50d5976def5a5b56bf548086da118
Victor Armas <victor.961004@outlook.com>
2024-12-12 16:50:24 -0600

    Version 2 de boarding System

--- actions/update-boarding-action.ts
+++ actions/update-boarding-action.ts
@@ -1,30 +1,30 @@
-"use server"
-import { prisma } from "@/src/lib/prisma";
-import { BoardingSchema } from "@/src/schema";
-import { Boarding } from "@prisma/client";
+// "use server"
+// import { prisma } from "@/src/lib/prisma";
+// import { BoardingSchema } from "@/src/schema";
+// import { Boarding } from "@prisma/client";
 
-// Función para actualizar un embarque en la base de datos
-export async function updateBoarding(boardingId: number, data: Boarding) {
-  // Validamos los datos antes de proceder
-  const result = BoardingSchema.safeParse(data);
-  if (!result.success) {
-    return {
-      errors: result.error.issues,
-    };
-  }
+// // Función para actualizar un embarque en la base de datos
+// export async function updateBoarding(boardingId: number, data: Boarding) {
+//   // Validamos los datos antes de proceder
+//   const result = BoardingSchema.safeParse(data);
+//   if (!result.success) {
+//     return {
+//       errors: result.error.issues,
+//     };
+//   }
 
-  try {
-    // Actualizamos el embarque en la base de datos usando el boardingId
-    const updatedBoarding = await prisma.boarding.update({
-      where: { id: boardingId },
-      data: result.data,
-    });
+//   try {
+//     // Actualizamos el embarque en la base de datos usando el boardingId
+//     const updatedBoarding = await prisma.boarding.update({
+//       where: { id: boardingId },
+//       data: result.data,
+//     });
 
-    // Retornamos el embarque actualizado
-    return { updatedBoarding };
-  } catch (error) {
-    return {
-      errors: [{ message: "Error al actualizar el embarque" }],
-    };
-  }
-}
+//     // Retornamos el embarque actualizado
+//     return { updatedBoarding };
+//   } catch (error) {
+//     return {
+//       errors: [{ message: "Error al actualizar el embarque" }],
+//     };
+//   }
+// }
