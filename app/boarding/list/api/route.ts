5dd8e17dc80c818bd404d0ed4afa82d072d2c21c
Victor Armas <victor.961004@outlook.com>
2024-12-12 17:07:35 -0600

    Version 2 de boarding System

--- app/boarding/list/api/route.ts
+++ app/boarding/list/api/route.ts
@@ -1,10 +1,20 @@
-import { BoardingWhereInput } from "@/src/types";
-import { PrismaClient, BoardingStatus } from '@prisma/client';
+import { PrismaClient, Boarding } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const BoardingStatus = {
+  PENDING_DOWNLOAD: "PENDING_DOWNLOAD",
+  DOWNLOADING: "DOWNLOADING",
+  VALIDATING: "VALIDATING",
+  CAPTURING: "CAPTURING",
+  COMPLETED: "COMPLETED",
+} as const;
+
+export type BoardingStatus = (typeof BoardingStatus)[keyof typeof BoardingStatus];
 
 export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
-    const prisma = new PrismaClient();
     const url = new URL(request.url);
     const boxNumber = url.searchParams.get("boxNumber");
     const arrivalDate = url.searchParams.get("arrivalDate");
@@ -13,16 +23,15 @@ export async function GET(request: Request) {
     const page = parseInt(url.searchParams.get("page") || "1", 10);
     const pageSize = 10;
 
-    const where: BoardingWhereInput = {};
+    const where: any = {}; // Ajusta tu tipo aquí si es necesario
 
-    if (boxNumber) where.boxNumber = { contains: boxNumber, mode: "insensitive" }; // Filtrado parcial
+    if (boxNumber) where.boxNumber = { contains: boxNumber, mode: "insensitive" }; 
     if (arrivalDate) where.arrivalDate = new Date(arrivalDate);
-    if (supplierName) where.supplier = { name: { contains: supplierName, mode: "insensitive",} };
+    if (supplierName) where.supplier = { name: { contains: supplierName, mode: "insensitive" } };
 
-    // Validar y asignar el valor del estado si está presente
     if (status && Object.values(BoardingStatus).includes(status as BoardingStatus)) {
         where.status = status as BoardingStatus;
-      }
+    }
 
     try {
         const totalRecords = await prisma.boarding.count({ where });
@@ -43,7 +52,7 @@ export async function GET(request: Request) {
             currentPage: page,
         });
     } catch (error) {
-        console.log("Error fetching boarding records:", error);
+        console.error("Error fetching boarding records:", error);
         return new Response("Error fetching data", { status: 500 });
     }
 }
